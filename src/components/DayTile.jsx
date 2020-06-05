64d3fa775ea34fd08f33281fd89f425b1ca8b0e9
idecay <greenwood.cm88@gmail.com>
2020-06-05 15:06:28 -0400

    complete MVP

--- src/components/DayTile.jsx
+++ src/components/DayTile.jsx
@@ -1,7 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
+import "../style/DayTile.css";
+var moment = require("moment");
 
-export default class DayTile extends Component {
-  render() {
-    return <div>hi</div>;
-  }
-}
+const DayCard = ({ reading }) => {
+  let newDate = new Date();
+  const weekday = reading.dt * 1000;
+  newDate.setTime(weekday);
+
+  const imgURL = `owf owf-${reading.weather[0].id} owf-5x`;
+
+  return (
+    <div className="col-sm-2">
+      <div className="card dayTile">
+        <h3 className="card-title tileTitle">
+          {moment(newDate).format("dddd")}
+        </h3>
+        <p className="text-muted dateText">
+          {moment(newDate).format("MMMM Do, h:mm a")}
+        </p>
+        <div className="weatherIcon">
+          <i className={imgURL}></i>
+        </div>
+        <h2 className="temp">{Math.round(reading.main.temp)} °F</h2>
+        <div className="card-body description">
+          <p className="card-text">{reading.weather[0].description}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DayCard;
